202f08039c6b6fb35da8ce12ca437ef1f2e8b7b4
Leonardo Machado Lino de Moura <leobil@gmail.com>
2019-07-04 23:55:11 -0300

    Versão Final Projeto Reserve

---
+++ Reserve.Console/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Reserve.ProjectConsole
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string lcMenu = "";
+
+            do
+            {
+                Console.Clear();
+
+                Console.WriteLine("Digite uma sequencia de 5 números (todos entre 0 e 100)!");
+                Console.WriteLine(":: Separe os números com vírgula ::");
+                Console.Write("=> ");
+                string lcTextoDigitado = Console.ReadLine();
+                string lcNumerosExtensos = "";
+                try
+                {
+                    List<int> laListaInteirosValidos = new List<int>();
+                    laListaInteirosValidos = clsUtil.fncValidaSequencia(lcTextoDigitado);
+                    clsListagens loListagem = new clsListagens();
+                    foreach(int laNumero in laListaInteirosValidos)
+                    {
+                        lcNumerosExtensos = lcNumerosExtensos + loListagem.fncRetornoNumeroExtenso(laNumero) + ", ";
+
+                    }
+
+                    lcNumerosExtensos = lcNumerosExtensos.Substring(0, lcNumerosExtensos.Length - 2);
+                    string lcNumerosExtensosTratados = lcNumerosExtensos.Replace(" ", "").Replace(",", "");
+                    int lnTotalLetras = lcNumerosExtensosTratados.Length;
+
+                    Console.WriteLine("");
+                    Console.WriteLine("****************************************");
+                    Console.WriteLine("Olha que legal: ");
+                    Console.WriteLine("Números digitados: " + lcTextoDigitado);
+                    Console.WriteLine("Números digitados por extenso: " + lcNumerosExtensos);
+                    Console.WriteLine("Total de letras: " + lnTotalLetras.ToString());
+                    Console.WriteLine("****************************************");
+                    Console.WriteLine("");
+                    Console.WriteLine("Vamos continuar? (S/N)");
+                    lcMenu = Console.ReadLine();
+
+
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("-----------------------------------------");
+                    Console.WriteLine(e.Message);
+                    lcMenu = Console.ReadLine();
+
+                }
+
+                 } while (lcMenu.ToUpper() != "N");
+            Console.WriteLine("");
+            Console.WriteLine("****************************************");
+            Console.WriteLine("Obrigado por brincar comigo.");
+            Console.WriteLine("****************************************");
+            Console.ReadKey();
+
+        }
+    }
+}
+
---
+++ Reserve.Console/clsUtil.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reserve.ProjectConsole
+{
+    public static class clsUtil
+    {
+        
+
+
+        public static List<int> fncValidaSequencia(string tcSequenciaDigitada)
+        {
+            List<int> laListaInteirosValidos = new List<int>();
+            try
+            {
+                string[] aListaValores = tcSequenciaDigitada.Split(char.Parse(","));
+                fncValidaArgumentosMinimos(aListaValores);
+                fncValidaArgumentosMaximos(aListaValores);
+                laListaInteirosValidos = fncValidaNumerosInteiros(aListaValores);
+                fncValidaArgumentosMinMax(laListaInteirosValidos);
+
+            }
+            catch(Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+          
+
+            return laListaInteirosValidos;
+        }
+
+
+
+        private static void fncValidaArgumentosMinimos(string[] taListaValores)
+        {
+            #region NumeroMinimoArgumento
+            //Validação se não tem o número correto de argumentos (5)
+            if (taListaValores.Count() < 5)
+                throw new ArgumentException("oi, tudo bem? Então, não consegui encontrar 5 valores. Vamos tentar novamente? (S/N)");
+            #endregion NumeroMinimoArgumento
+        }
+
+        private static void fncValidaArgumentosMaximos(string[] taListaValores)
+        {
+            #region NumeroMaximoArgumento
+            //Validação se não tem um número maior do que o sugerido (5)
+            if (taListaValores.Count() > 5)
+                throw new ArgumentException("Então... como vai? Eu encontrei mais de 5 valores. Vamos tentar novamente? :)  (S/N)");
+            #endregion NumeroMaximoArgumento
+        }
+
+        private static List<int> fncValidaNumerosInteiros(string[] taListaValores)
+        {
+            #region NumerosInteirosValidos
+            //Validação se todos os valores são inteiros válidos;
+            string lcListaValoresErrados = "";
+            bool lbExisteValorErrado = false;
+            List<int> aListaValoresConvertidos = new List<int>();
+            foreach (string laValor in taListaValores)
+            {
+                int loValorTestado;
+                bool lbResult = Int32.TryParse(laValor, out loValorTestado);
+                if (lbResult)
+                {
+                    aListaValoresConvertidos.Add(loValorTestado);
+                }
+                else
+                {
+                    lbExisteValorErrado = true;
+                    lcListaValoresErrados = lcListaValoresErrados + laValor + ", ";
+                }
+            }
+
+            if (lbExisteValorErrado)
+            {
+                lcListaValoresErrados = lcListaValoresErrados.Substring(0, lcListaValoresErrados.Length - 2);
+                throw new ArgumentException("Olá! Fiz alguns testes e acho que não vou conseguir trabalhar com os seguintes valores digitados: \n" + lcListaValoresErrados + ". Vamos tentar novamente? ");
+            }
+            #endregion NumerosInteirosValidos
+
+            return aListaValoresConvertidos;
+        }
+
+        public static void fncValidaArgumentosMinMax(List<int> taListaValores)
+        {
+            //Validação se todos os valores são inteiros acima de zero e menor que 100;
+            string lcListaValoresErrados = "";
+            bool lbExisteValorErrado = false;
+            foreach (int laNumero in taListaValores)
+            {
+                if (laNumero < 0 || laNumero > 100)
+                {
+                    lbExisteValorErrado = true;
+                    lcListaValoresErrados = lcListaValoresErrados + laNumero.ToString() + ", ";
+                }
+            }
+
+            if (lbExisteValorErrado)
+            {
+                lcListaValoresErrados = lcListaValoresErrados.Substring(0, lcListaValoresErrados.Length - 2);
+                throw new ArgumentException("Olá! Fiz alguns testes e acho que não vou conseguir trabalhar com os seguintes valores digitados: \n" + lcListaValoresErrados + ". \n Vamos tentar novamente? E desta vez pode digitar números maiores que 0 (zero) e menores que 100 (cem)? ");
+            }
+
+        }
+    }
+}
